7276c8a039104625f706d04535aedf00b4d66f65
D-MAS <57054077+D-MAS@users.noreply.github.com>
2021-01-05 07:16:46 -0600

    Added roundcrt by 1-239
    also has a Wlv = PCC option

--- FE4-critical.js
+++ FE4-critical.js
@@ -21,3 +21,31 @@
 
 		return this.validValue(active, passive, weapon, damage);
 	}
+
+	var PCC_MODE = 0;
+	// 0: uses custom.roundcrt
+	// 1: uses Wlv
+	// -1: disabled
+
+	// roundcount critical multiplier script by 1-239
+	AttackEvaluator.HitCritical.calculateCritical = function(virtualActive, virtualPassive, attackEntry) {
+		var percent = CriticalCalculator.calculateCritical(virtualActive.unitSelf, virtualPassive.unitSelf, virtualActive.weapon, virtualActive.totalStatus, virtualPassive.totalStatus);
+		
+		var activecount = Calculator.calculateRoundCount(virtualActive.unitSelf, virtualPassive.unitSelf, virtualActive.weapon);
+
+		if(activecount == 2) {
+			if(virtualActive.roundCount == 0) {
+				if (PCC_MODE === 0) {
+					if(typeof virtualActive.unitSelf.custom.roundcrt == 'number') {
+						percent　*= virtualActive.unitSelf.custom.roundcrt;
+					}
+				// support for using Wlv as PCC
+				} else if (PCC_MODE === 1) {
+					percent　*= RealBonus.getWlv(virtualActive.unitSelf);
+				}
+			}
+		}
+		root.log("Critical: " + percent + "%");
+		return Probability.getProbability(percent);
+
+	}
